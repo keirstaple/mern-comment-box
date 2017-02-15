c09594ea1a75ee0932211cb92ac798255a8f131e
Keir Staple <keirstaple@comcast.net>
2017-02-15 12:10:53 -0800

    add get request to server

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import style from './style';
@@ -7,13 +8,29 @@ class CommentBox extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [] };
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+  loadCommentsFromServer() {
+    axios.get(this.props.url)
+      .then(res => {
+        this.setState({ data: res.data });
+        console.log('server data: ' + this.state.data);
+      })
+  }
+  handleCommentSubmit(comment) {
+    //add POST request
+  }
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   }
   render() {
     return (
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
-        <CommentList data={ this.state.data } />
-        <CommentForm />
+        <CommentList data={ this.state.data }/>
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
       </div>
     )
   }

d8c297810632b8074e6e1cdea7e4f9bb5a310cdd
Keir Staple <keirstaple@comcast.net>
2017-02-15 12:00:58 -0800

    add first post and get methods to database

--- server.js
+++ server.js
@@ -41,6 +41,32 @@ router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!'});
 });
 
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments)
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
+
 //Use our router configuration when we call /api
 app.use('/api', router);
 
